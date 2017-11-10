f7e0b8b976ef9747bd037f5c02418204a3c0b931
kagose yusuke <>
2017-11-10 16:47:38 +0900

    install kaminari

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'faker', '~> 1.8', '>= 1.8.4'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'jquery-rails'
+gem 'kaminari', '~> 1.1', '>= 1.1.1'
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :group
+
+  paginates_per 10
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
 end
