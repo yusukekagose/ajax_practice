8894b295855fd448bda5819595d6d7e852b8f309
kagose yusuke <>
2017-11-11 11:29:26 +0900

    update avatar!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require jasny-bootstrap.min
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110015337) do
+ActiveRecord::Schema.define(version: 20171111020746) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20171110015337) do
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["group_id"], name: "index_contacts_on_group_id"
   end
 
