7031386160e27ba848e10ff6054f52c18d9bc29b
kagose yusuke <>
2017-11-11 10:49:26 +0900

    edit funciton

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,6 +21,20 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      flash[:success] = "Contact was successfully updated"
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
+  end
+
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   get 'contacts/index', as: 'contacts'
   get 'contacts/new', as: 'new_contact'
-  post '/contacts/create'
+  post '/contacts/create', as: 'create_contact'
+  get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+  patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
   root 'contacts#index'
 
 end
