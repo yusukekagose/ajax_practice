b4d5d27439f32ecbf92e979f9bbff2aac9f1da99
kagose yusuke <>
2017-11-11 21:00:02 +0900

    active nav helper funtion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def active_item(uri)
+    uri_segments = request.fullpath.split(/\/|\?/)
+    uri_segments[1] === uri ? 'active' : ''
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+
+  get 'dashboard/index'
 
   devise_for :users
   resources :contacts, except: [:show] do
@@ -7,6 +10,8 @@ Rails.application.routes.draw do
   end
 
   post '/groups', to: 'groups#create'
+
+  get '/dashboard', to: 'dashboard#index'
   root 'contacts#index'
 
 end
