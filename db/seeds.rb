25fd8aac844e0d19f762961922b945c71bab6cdf
kagose yusuke <>
2017-11-11 21:40:33 +0900

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Contact.destroy_all
 Group.destroy_all
+User.destroy_all
 
-group_ids = []
+user_ids = []
+user_ids << User.create(name: "John Doe", email: "johndoe@test.com", password: "secret").id
+user_ids << User.create(name: "Jane Roe", email: "janeroe@test.com", password: "secret").id
 
-group_ids << Group.create(name: "Client").id
-group_ids << Group.create(name: "Friend").id
-group_ids << Group.create(name: "Family").id
+p "2 users created"
 
-p "#{group_ids.count} created"
+group_ids = { user_ids[0] => [], user_ids[1] => [] }
+
+group_ids[user_ids[0]] << Group.create(name: "Client", user_id: user_ids[0]).id
+group_ids[user_ids[0]] << Group.create(name: "Friend", user_id: user_ids[0]).id
+group_ids[user_ids[1]] << Group.create(name: "Family", user_id: user_ids[1]).id
+group_ids[user_ids[1]] << Group.create(name: "Company", user_id: user_ids[1]).id
+
+p "#{group_ids.count} group created"
 
 group_count = group_ids.length
-number_of_contacts = 20
+number_of_contacts = 60
 contacts = []
 
 number_of_contacts.times do |i|
-  new_contact = {
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    company: Faker::Company.name,
-    phone: Faker::PhoneNumber.cell_phone,
-    address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
-    group_id: group_ids[Random.rand(0...group_count)]
-  }
-  contacts.push(new_contact)
+    user_id = user_ids[Random.rand(0...2)]
+
+    new_contact = {
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        company: Faker::Company.name,
+        phone: Faker::PhoneNumber.cell_phone,
+        address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
+        group_id: group_ids[user_id][Random.rand(0...group_count)],
+        user_id: user_id
+    }
+    contacts.push(new_contact)
 end
 
 Contact.create(contacts)
