968b13f2d101cb77f52df42749fbc461a30a4e63
kagose yusuke <>
2017-11-11 14:29:23 +0900

    flexed the selected group

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,8 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:edit, :update, :destroy]
+
   def index
+    session[:selected_group_id] = params[:group_id]
     if params[:group_id] && !params[:group_id].empty?
       @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
     else
@@ -15,28 +18,26 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Contact was successfully created"
-      redirect_to contacts_path
+      redirect_to contacts_path(previous_query_string)
     else
       render 'new'
     end
   end
 
   def edit
-    @contact = Contact.find(params[:id])
   end
 
   def update
-    @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
       flash[:success] = "Contact was successfully updated"
-      redirect_to contacts_path
+      redirect_to contacts_path(previous_query_string)
     else
       render 'edit'
     end
   end
 
   def destroy
-    @contact = Contact.find(params[:id]).destroy
+    @contact.destroy
     flash[:success] = "Contact was successfully deleted"
     redirect_to contacts_path
   end
@@ -44,5 +45,13 @@ class ContactsController < ApplicationController
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id, :avatar)
+    end
+
+    def find_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    def previous_query_string
+      session[:selected_group_id] ? { group_id: session[:selected_group_id] } : {}
     end
 end
